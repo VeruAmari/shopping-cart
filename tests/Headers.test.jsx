742016635099de1b67b0ca9627ee95b8b3cef859
Veronica <43966969+VeruAmari@users.noreply.github.com>
2024-03-28 14:34:00 -0300

    Write header level tests

--- tests/Headers.test.jsx
+++ tests/Headers.test.jsx
@@ -7,12 +7,22 @@ describe('Heading', () => {
     render(<Heading level="1">Level 1 Heading</Heading>);
     expect(screen.getByText('Level 1 Heading').textContent).toMatch(/level/i);
   });
-  it('correctly handles heading level', () => {
+  it('correctly handles heading levels', () => {
     render(
-      <div data-testid="parent">
-        <Heading level="1"></Heading>
-      </div>,
+      <>
+        <div data-testid="parent">
+          <Heading level="1">Heading</Heading>
+        </div>
+        <div data-testid="parent-2">
+          <Heading level="2">Heading</Heading>
+        </div>
+        <div data-testid="parent-3">
+          <Heading level="6">Heading</Heading>
+        </div>
+      </>,
     );
-    expect(screen.getByTestId('parent')).toContainHTML('<h1></h1>');
+    expect(screen.getByTestId('parent').firstChild).toContainHTML('</h1>');
+    expect(screen.getByTestId('parent-2').firstChild).toContainHTML('</h2>');
+    expect(screen.getByTestId('parent-3').firstChild).toContainHTML('</h6>');
   });
 });
