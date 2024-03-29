578f05ce73454b35efe5cce761b3ece8cf3d4c8a
Veronica <43966969+VeruAmari@users.noreply.github.com>
2024-03-29 16:00:19 -0300

    Remove duplicate import declaration

--- src/components/main/Navbar.jsx
+++ src/components/main/Navbar.jsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { NavElement } from '../sub/Containers';
 import Logo from './Logo';
-import { NavElement } from '../sub/Containers';
 
 const Navbar = () => {
   return (
