d5ccf323e61389d2eab9508264a12cd356ea57e7
Veronica <43966969+VeruAmari@users.noreply.github.com>
2024-04-07 12:39:53 -0300

    test: write tests for margin

--- tests/Headers.test.jsx
+++ tests/Headers.test.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable no-undef */
 import { render, screen } from '@testing-library/react';
 
-import { Heading } from '../src/components/sub/Headers';
+import Heading from '../src/components/sub/Headers';
 
 describe('Heading', () => {
   it('renders', () => {
@@ -24,5 +25,37 @@ describe('Heading', () => {
     expect(screen.getByTestId('parent').firstChild).toContainHTML('</h1>');
     expect(screen.getByTestId('parent-2').firstChild).toContainHTML('</h2>');
     expect(screen.getByTestId('parent-3').firstChild).toContainHTML('</h6>');
+  });
+
+  it('correctly sets margins', () => {
+    render(
+      <>
+        <Heading margin="2">Heading with a margin of 2</Heading>
+        <Heading margin="0">Heading with a margin of 0</Heading>
+        <Heading margin=".2">Heading with a margin of 0.2</Heading>
+        <Heading>Heading with a default margin</Heading>
+        <Heading margin="a">Heading margin ignores a string value</Heading>
+        <Heading margin={null}>Heading margin ignores a null value</Heading>
+      </>,
+    );
+    expect(screen.getByText('Heading with a margin of 2').style.margin).toMatch(
+      '2rem',
+    );
+    expect(screen.getByText('Heading with a margin of 0').style.margin).toMatch(
+      '0',
+    );
+    expect(
+      screen.getByText('Heading with a margin of 0.2').style.margin,
+    ).toMatch('.2rem');
+
+    expect(
+      screen.getByText('Heading with a default margin').style.margin,
+    ).toMatch('.5rem');
+    expect(
+      screen.getByText('Heading margin ignores a string value').style.margin,
+    ).toMatch('.5rem');
+    expect(
+      screen.getByText('Heading margin ignores a null value').style.margin,
+    ).toMatch('.5rem');
   });
 });
