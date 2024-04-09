1eaad9dfa372d811eedfc9e69af1deccd1b9b090
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-04-09 17:25:14 -0300

    test: add userEvent test

--- tests/Containers.test.jsx
+++ tests/Containers.test.jsx
@@ -4,6 +4,7 @@ import { MemoryRouter } from 'react-router-dom';
 // import userEvent from "@testing-library/user-event";
 
 import { NavElement } from '../src/components/sub/Containers';
+import userEvent from '@testing-library/user-event';
 
 describe('NavElement', () => {
   it('renders', () => {
@@ -22,5 +23,15 @@ describe('NavElement', () => {
       </MemoryRouter>,
     );
     expect(screen.getByText('Cart')).toMatchSnapshot();
+  });
+  it('is clickable', async () => {
+    const user = userEvent.setup();
+    render(
+      <MemoryRouter>
+        <NavElement to="/cart">Cart</NavElement>
+      </MemoryRouter>,
+    );
+
+    await user.click(screen.getByText('Cart'));
   });
 });
