3838e2a90fea43cce6233bf3a92408e61493c97e
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-04-09 20:19:15 -0300

    test: add userEvent testing for card buttons

--- tests/Card.test.jsx
+++ tests/Card.test.jsx
@@ -1,6 +1,7 @@
 import Card from '../src/components/sub/Card';
 import { useState } from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 const product = {
   id: 0,
@@ -10,6 +11,7 @@ const product = {
   description: 'A delicious acorn for coffee grind or a meal.',
   image: 'img',
 };
+
 const CardContainer = () => {
   const [cartProducts, setCartProducts] = useState('');
   return (
@@ -26,9 +28,29 @@ const CardContainer = () => {
     />
   );
 };
+
 describe('Card component', () => {
   it('renders', () => {
     render(<CardContainer />);
     expect(screen.getByText(product.title)).toBeInTheDocument();
+  });
+
+  it('buttons are clickable', async () => {
+    const user = userEvent.setup();
+
+    render(<CardContainer />);
+    await user.click(screen.getByText('+'));
+    await user.click(screen.getByText('-'));
+  });
+
+  it('buttons correctly increase/decrease product count', async () => {
+    const user = userEvent.setup();
+
+    render(<CardContainer />);
+    await user.click(screen.getByText('+'));
+    await user.click(screen.getByText('-'));
+    await user.click(screen.getByText('+'));
+
+    expect(screen.getByRole('textbox').value).toBe('1');
   });
 });
