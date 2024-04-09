68ce28f20fe3bccb3b35af9d62332cf26196fa49
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-04-09 12:24:18 -0300

    feat: add children handling for easier testing

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,14 +1,20 @@
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Heading from './components/sub/Headers';
 import { PageContainer } from './components/main/App';
 
-const ErrorPage = () => {
+const ErrorPage = ({ children }) => {
   return (
     <PageContainer>
       <Heading level="1">Oh no! This route doesn&apos;t exist!</Heading>
+      {children}
       <Link to="/">You can go back to the home page by clicking here.</Link>
     </PageContainer>
   );
+};
+
+ErrorPage.propTypes = {
+  children: PropTypes.any,
 };
 
 export default ErrorPage;
